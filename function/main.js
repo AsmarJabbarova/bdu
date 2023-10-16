ad5647d164d6822faae2d5273996b60fd1610705
AsmarJabbarova <145671712+AsmarJabbarova@users.noreply.github.com>
2023-10-16 19:52:58 +0400

    Add files via upload

---
+++ function/main.js
@@ -0,0 +1,83 @@
+// function getSum(num1, num2) {
+//   sum=num1+num2
+//     console.log(sum) 
+//     return getSum
+// }
+// function getDif(num1, num2) {
+//    dif=num1-num2                             
+//     console.log(dif)
+//     return getDif
+// }
+// function getProduct(getDif, getSum) {
+//     console.log(getDif * getSum)
+// }
+
+// let obj = {
+//     name:"Asmar",
+//     surname:"Jabbarova",
+//     age:19,
+// }
+// function data{
+//     return obj.name=" "+obj.surname+" "+obj.age;
+// }
+// console.log(data(obj))
+
+// Task1-- Daxil edilmis ededin sade/murekkeb olmasini WebTransportBidirectionalStream.Eger sadedirse true, murrekkebdirse false qaytarmalidir. 1 dirs 0 return edin
+// function IsPrime(n) {
+//     let divisionCount = 0;
+//     for (let i = 1; i <=n; i++) {
+//         if (n % i == 0) {
+//             divisionCount++
+//         }
+
+//     }
+//     if (divisionCount > 2)
+//         return false;
+//     else if (n == 1) 
+//         return 0
+
+    
+//     else 
+//         return true;
+    
+
+// }
+// // console.log(IsPrime(23))
+
+
+
+
+// let arr = [7,6,5,4,8]
+// function PrimeQuantity(arr){
+//     let count = 0;
+//     for ( let i = 0; i < arr.length; i++){
+//         if (IsPrime (arr[i]) ){
+//             count++
+
+//         }
+//     }
+//     return count;
+// }
+
+// // console.log(PrimeQuantity(arr));
+
+
+const car = {
+    brand:"Mercedes",
+    model:"s class",
+    year:2022,
+    mileage: 10500,
+    fuelfor1KM:5,
+    fuelCapacity:15,
+    curresntFuel:10,
+    IncreaseFuel : function(){
+        if(this.curresntFuel+this)
+        return(`${this.increaseFuel} ${this.drive}`)
+    },
+    findIncreaseFuel
+
+}
+
+
+
+
