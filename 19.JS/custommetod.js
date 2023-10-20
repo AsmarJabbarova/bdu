9860e30856720329e4d0a50a20ff68ef64adad88
AsmarJabbarova <145671712+AsmarJabbarova@users.noreply.github.com>
2023-10-20 19:57:22 +0400

    Add files via upload

---
+++ 19.JS/custommetod.js
@@ -0,0 +1,46 @@
+// // 4- capatalize custom Method
+// String.prototype.myCaptalize= function (){
+//     for (let i = 0; i < this.length; i++){
+//         return this[0].toUpperCase()+this.slice(1)
+//     }
+// }
+// console.log("salam".myCaptalize());
+
+
+
+
+// // 5-isBlank custom method -> ("   ")-true, ("")-false, ("  s  ")->false
+// String.prototype.isBlank= function (){
+// for (let i = 0; i < this.length; i++){
+//     if(this[i]!=` `){
+//         return false
+//     }
+  
+// }
+// return true
+// }
+// let str=`   sbbhj `
+// console.log(str.isBlank())
+
+
+
+// // 6-custom concat Metod -> salam.(`sagol`) -> (`salamsagol`)
+// String.prototype.concat= function (str){
+// for (let i = 0; i < this.length; i++){
+//    return this+ str
+// }
+
+// }
+// console.log(`salam`.concat(`sagol`));
+
+
+
+
+// 7-custom replace Metod ->sulum (`u` ,`a`) ->salam         
+String.prototype.replace= function (){
+for (let i = 0; i < this.length; i++){
+
+}
+
+
+}
